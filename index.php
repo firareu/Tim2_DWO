e1545fcc7df2a4d4c67768e9d2779168349022c2
wisnu-ace <77220097+wisnu-ace@users.noreply.github.com>
2023-06-06 16:17:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -213,7 +213,7 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Production</div>
+                                            Total Inventory Quantity</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                                 <?php
                                                 include 'koneksi.php';
@@ -238,8 +238,15 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Total Sales</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Total Purchase Order Quantity</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            <?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT SUM(`orderqty`) AS total FROM factpurchase");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -255,19 +262,15 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Order (qty)
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Purchase Unit Price
                                             </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            <?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT SUM(`unitprice`) AS total FROM factpurchase");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?>
                                             </div>
                                         </div>
                                         <div class="col-auto">
@@ -285,8 +288,16 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Total Net Income</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                                Total Vendor</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            <?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT COUNT(DISTINCT VendorID) AS total FROM factpurchase;");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?>
+                                            </div>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -303,8 +314,15 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Total Purchase</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Total Sales Order Quantity</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            <?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT SUM(`orderqty`) AS total FROM factsales");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -321,8 +339,15 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Total Vendor</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Total Sales Unit Price</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            <?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT SUM(`unitprice`) AS total FROM factsales");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -339,8 +364,13 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Total Unit</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                                Total Customer</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
+                                                include 'koneksi.php';
+                                                $result = mysqli_query($conn, "SELECT COUNT(DISTINCT customerID) AS total FROM factsales;");
+                                                $row = mysqli_fetch_assoc($result);
+                                                echo '$' . number_format($row['total'], 2);
+                                                ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-comments fa-2x text-gray-300"></i>
