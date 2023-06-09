48edb9ab09444c898c5ef610a21564808d2ad727
Putri Dian S <91788765+firareu@users.noreply.github.com>
2023-06-10 01:04:52 +0700

    baru perubahan tampilan purchase+copas line & pie

---
+++ mainpurchase.php
@@ -0,0 +1,118 @@
+<div id="content" class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Purchase</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Purchase</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <?php
+                                require 'koneksi.php';
+                                $query = "SELECT SUM(Quantity) AS totalInventoryQty FROM `factinventory`";
+                                $result = mysqli_query($conn, $query);
+                                if ($result) {
+                                    $row = mysqli_fetch_assoc($result);
+                                    $totalInventoryQty = $row['totalInventoryQty'];
+                                    echo  $totalInventoryQty;
+                                } else {
+                                    echo "Tidak ada data yang ditemukan.";
+                                }
+                                ?>
+                            </div>
+                    </div>
+                        <div class="col-auto">
+                            <i class="fas fa-solid fa-file-invoice-dollar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Total Vendor</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    <?php
+                                    require 'koneksi.php';
+                                    $query = "SELECT SUM(Quantity) AS totalInventoryQty FROM `factinventory`";
+                                    $result = mysqli_query($conn, $query);
+                                    if ($result) {
+                                        $row = mysqli_fetch_assoc($result);
+                                        $totalInventoryQty = $row['totalInventoryQty'];
+                                        echo  $totalInventoryQty;
+                                    } else {
+                                        echo "Tidak ada data yang ditemukan.";
+                                    }
+                                    ?>
+                                </div>
+                        </div>
+                            <div class="col-auto">
+                                <i class="fas fa-solid fa-store fa-2x text-gray-300"></i>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Income Average</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    <?php
+                                    require 'koneksi.php';
+                                    $query = "SELECT SUM(Quantity) AS totalInventoryQty FROM `factinventory`";
+                                    $result = mysqli_query($conn, $query);
+                                    if ($result) {
+                                        $row = mysqli_fetch_assoc($result);
+                                        $totalInventoryQty = $row['totalInventoryQty'];
+                                        echo  $totalInventoryQty;
+                                    } else {
+                                        echo "Tidak ada data yang ditemukan.";
+                                    }
+                                    ?>
+                                </div>
+                        </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <!-- Area Chart -->
+        <?php include 'linepurchase.php' ?>
+        <!-- Pie Chart -->
+        <?php include 'piepurchase1.php' ?>
+        
+    </div>
+    <div class="row">
+        <!-- Area Chart -->
+        <?php include 'barpurchase.php' ?>
+        <!-- Pie Chart -->
+        <?php include 'piepurchase2.php' ?>
+        
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
